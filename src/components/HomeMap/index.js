5b0aec1bcaa6c46244e1dd63cd2435dc74e5cb0b
Domagoj Marusic <bradazg@gmail.com>
2021-01-10 00:00:27 +0100

    Added Home screen and Destination search with Google Places Autocomplete

---
+++ src/components/HomeMap/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const HomeMap = () => {
+  return (
+    <View
+      style={{
+        height: 280,
+        backgroundColor: '#A0ABFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Map</Text>
+    </View>
+  );
+};
+
+export default HomeMap;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react';
+import {View, TextInput, SafeAreaView} from 'react-native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {API_KEY} from '@env';
+
+import styles from './styles';
+
+const DestinationSearch = () => {
+  const [originPlace, setOriginPlace] = useState(null);
+  const [destinationPlace, setDestinationPlace] = useState(null);
+
+  useEffect(() => {
+    console.warn('useEffect is called');
+    if (originPlace && destinationPlace) {
+      console.warn('Redirect to results');
+    }
+  }, [originPlace, destinationPlace]);
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          onPress={(data, details = null) => {
+            setOriginPlace({data, details});
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: API_KEY,
+            language: 'en',
+          }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where to?"
+          onPress={(data, details = null) => {
+            setDestinationPlace({data, details});
+          }}
+          styles={{
+            textInput: styles.textInput,
+          }}
+          fetchDetails
+          query={{
+            key: API_KEY,
+            language: 'en',
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default DestinationSearch;
